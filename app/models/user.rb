3608859f179d6cffdcb148c02eb6ec0e47d969f2
Alejandro Piñango <alejandropinango@gmail.com>
2023-10-12 19:17:37 -0300

    Registration and application controller modified. User role added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Enum de roles
+  enum role: { user: 'user',
+                admin: 'admin',
+  }, _default: 'user'
+  
+  # Validaciones
+  validates :username, presence: { message: "Username field cannot be blank" }, uniqueness: { message: "Username is already in use" }
+
 end
